d77fbe9d894c78f5df2890b3fb42068656965d4a
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-04-06 09:58:05 -0400

    finalized code, added comments to html css and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 
 var saveBtn = document.querySelector(".saveBtn");
-const currentTime = dayjs().$H;
+const currentTime = dayjs().$H; //sets the current time for the timeblock function
 var description = document.getElementsByClassName(".description");
-const today = dayjs();
+const today = dayjs(); //displays the date on the header of the page
 $('#currentDay').text(dayjs().format('dddd MMMM D, YYYY'));
 
-$(function () {
+$(function () {//function for the timeblock, adds relevent class for "past present future" according to the current time 
   const timeBlock = document.querySelectorAll(".time-block");
 
-  timeBlock.forEach(block => {
+  timeBlock.forEach(block => {//this "for each" creates a block from timeblock and grabs the id and splits them at the hyphen
     const timeBlockHour = block.getAttribute("id").split("-");
 
     if (parseInt(timeBlockHour[1]) < currentTime) {
@@ -23,18 +23,18 @@ $(function () {
 
 $(document).ready(function () {
 
-  $(".saveBtn").on("click", function (event) {
+  $(".saveBtn").on("click", function (event) {//.ready function for the save button event for click
     event.preventDefault();
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
 
-    localStorage.setItem(time, text);
+    localStorage.setItem(time, text);//sets above declared var to the local storage
   })
   
 });
 
-window.onload = function loadContent() {
+window.onload = function loadContent() {//window onload function of load content grabs the values in each specific id and displays them on the page 
 
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
